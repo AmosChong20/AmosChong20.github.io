159b5e9904ca8127e14784bd46f0a8524121fb74
AmosChong <amoschong0820@gmail.com>
2023-02-05 21:23:53 +0800

    New version to be updated

--- README.md
+++
@@ -1,5 +0,0 @@
-# My safari startpage
-
-## Programming language: HTML, CSS
-
-Build using flexbox and media queries to create a responsive startpage for my safari browser
--- index.js
+++
@@ -1,7 +0,0 @@
-const workspaceBtn = document.querySelector(".eql-btn");
-
-const workspaceul = document.querySelector(".workspaceul");
-
-workspaceBtn.addEventListener("click", e => {
-    workspaceul.classList.toggle("show-workspaceul");
-})
